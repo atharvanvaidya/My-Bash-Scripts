4e05adacb057ed4155894738704894573d83aa98
Atharva Vaidya <atharvanvaidya@gmail.com>
2018-01-20 14:30:07 +0530

    Basic Downloader

---
+++ lme.sh
@@ -0,0 +1,8 @@
+# /bin/bash
+
+wget http://sh.st/st/16c0541ba03b1429f0755923d0b22544/http://s3.ap-south-1.amazonaws.com/dl3.down2hub.com/Laakhon%20Mein%20Ek%20Episode%201%20Amazon%20Prime%20%5BDown2Hub.com%5D.mp4
+wget http://sh.st/st/16c0541ba03b1429f0755923d0b22544/http://s3.ap-south-1.amazonaws.com/dl3.down2hub.com/Laakhon%20Mein%20Ek%20Episode%202%20Amazon%20Prime%20%5BDown2Hub.com%5D.mp4
+wget http://sh.st/st/16c0541ba03b1429f0755923d0b22544/http://s3.ap-south-1.amazonaws.com/dl3.down2hub.com/Laakhon%20Mein%20Ek%20Episode%203%20Amazon%20Prime%20%5BDown2Hub.com%5D.mp4
+wget http://sh.st/st/16c0541ba03b1429f0755923d0b22544/http://s3.ap-south-1.amazonaws.com/dl3.down2hub.com/Laakhon%20Mein%20Ek%20Episode%204%20Amazon%20Prime%20%5BDown2Hub.com%5D.mp4
+wget http://sh.st/st/16c0541ba03b1429f0755923d0b22544/http://s3.ap-south-1.amazonaws.com/dl3.down2hub.com/Laakhon%20Mein%20Ek%20Episode%205%20Amazon%20Prime%20%5BDown2Hub.com%5D.mp4
+wget http://sh.st/st/16c0541ba03b1429f0755923d0b22544/http://s3.ap-south-1.amazonaws.com/dl3.down2hub.com/Laakhon%20Mein%20Ek%20Episode%206%20Amazon%20Prime%20%5BDown2Hub.com%5D.mp4
